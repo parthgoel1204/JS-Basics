017940ae46ba33dacdd0a25df1a0ba7bc1285298
Parth Goel <parthgoel1204@gmail.com>
2026-01-13 23:17:54 +0530

    update: shortest js program

--- file.js
+++ file.js
@@ -28,3 +28,19 @@ fs.readFile("./contacts.txt","utf-8" , (err , result) => {
 // fs.unlinkSync("./test2.txt");
 
 console.log(fs.statSync("./test.txt"));
+
+// get to know about shortest JS program that is a empty file 
+// It still has a window object and we are able to access all the functions in the global space and this functionality is given to us by Javascript Engine 
+// JS also creates "this" keyword and at the global level "this" points to the "window" object 
+
+// this === window at the global level thst is in the browser 
+
+// Global Space: if we write anything outside the function that is what we call as "Globla Space".
+
+var a = 10;
+console.log(window.a);
+console.log(a);
+console.log(this.a);
+
+// above written both statements logs the same output as in global space JS engine knows that weare referring to window object 
+// So, SHORTEST PROGRAM in the JS is an empty file.
